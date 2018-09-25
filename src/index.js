c6e8f69506757c2a5695bd9cddeb4cad3e68795f
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-09-26 04:03:24 +0800

    until subscriptions

--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
+// resolvers
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload')
+const Subscription = require('./resolvers/Subscription')
 
-let idCount = links.length
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-  },
-  Mutation: {
-    // 2
-    post: (root, args) => {
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    }
-  },
+  Query,
+  Mutation,
+  AuthPayload,
+  Subscription,
 }
 
 // 3
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/ronna-mae-firmo-1aec2b/database/dev',
+      secret: 'mysecret123',
+      debug: true
+    })
+  })
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
