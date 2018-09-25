1ed07acee23b9a619f5568f23481056bba534543
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-09-26 04:32:39 +0800

    filter, pagination

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,14 @@
 function feed(parent, args, context, info) {
-	return context.db.query.links({}, info)
+	const where = args.filter
+		? { OR: [
+			{ url_contains: args.filter },
+			{ description_contains: args.filter }
+		]}
+		: {}
+
+	return context.db.query.links({ 
+		where, skip: args.skip, first: args.first
+	}, info)
 }
 
 module.exports = {
